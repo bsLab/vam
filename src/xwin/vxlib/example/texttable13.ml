43c137b90e46f06787c2f1e391736ac7a5e00651
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 17:50:08 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/xwin/vxlib/example/texttable13.ml
@@ -0,0 +1,127 @@
+open X
+open VX_types
+open VX_box
+open VX_text
+open VX_texttable
+open VX_ps 
+
+let display = new VX_display.t ""
+let root = new VX_root.t display 0 
+let top = new VX_wmtop.t root      
+          [MinHeight 200; MinWidth 300]
+
+let vbox = new VX_box.v top#container 
+                            [
+                                Border [];
+                                Background "lightblue";
+                                IpadX 10;
+                                IpadY 10;
+                                ExpandX true;
+                                ExpandY true;
+                                AdjustY true;
+                            ] 
+
+let hbox = new VX_box.h vbox#container 
+                            [
+                                AdjustX true;
+                                ExpandX true;
+                                Background "lightblue";
+
+                            ] 
+
+let str = "This is a multiline paragraphe with several lines and words..."
+
+
+let info_tab = 
+[|
+    [| "Name";"Value";|];
+    [| "Col1";"Col2";|];
+    [| "Col3";"Col3";|];
+    [| "Col5";"Col6";|];
+|]
+
+let attr = [
+    Background "grey90";
+    IpadX 10;
+    IpadY 5;
+
+    Rows [|
+        [IpadY 5];
+        [];
+        [];
+        [];
+    |];
+    Cols [| 
+        [| [Border[]]; [Border[]] |];
+        [| [Border[]]; [Border[]] |];
+        [| [Border[]]; [Border[]] |];
+        [| [Border[]]; [Border[]] |];
+    |];
+]
+
+let info_tab = new VX_texttable.t hbox#container info_tab attr  
+
+let tab_cont = [|
+    [| "Col1";"Col2";|];
+    [| "Col3";str;|];
+    [| "Col5";"Info";|];
+|]
+
+
+let attr = [
+    Background "white";
+    IpadX 4;
+    IpadY 4;
+    Cols [|
+        [| 
+            [Border [Sides [B_left;B_top;B_right]];
+             Width 80;];
+            [Border [Sides [B_top;B_right]];
+             Width 50;];
+        |];
+        [| 
+            [Align Top; Border [Sides [B_left;B_top;B_right]];
+             Width 50;Text_style Bold];
+            [Rown 0; Border [Sides [B_top;B_right;]];
+             Width 80;Mutable true];
+        |];
+        [| 
+            [
+             Border [Sides [B_left;B_top;B_right;B_bottom]];
+             Width 50;Text_style Bold];
+            [
+             Border [Sides [B_top;B_right;B_bottom]];
+             Width 80;
+             But [Widget info_tab#contained];
+            ];
+        |];
+    
+
+    
+    |];
+]
+
+let tab = new VX_texttable.with_root hbox#container root tab_cont attr  
+
+let _ =
+    hbox#container_add_s [tab#contained];
+    vbox#container_add_s [hbox#contained];
+
+    top#setWM_NAME "VX test";
+    top#container_add vbox#contained;
+    top#show;
+
+    let printed = ref false in
+    top#configure [Bindings [
+        EnterWindow,(fun _ ->
+            if not !printed then
+            begin
+                printed := true;
+                print_eps "texttable13.eps" 1.0 vbox#contained;
+            end;
+            );   
+        ]];   
+
+    try
+      loop ();
+    with Exit -> ();
