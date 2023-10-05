c7ba509c2b8f56a23ad8ba66775129c6f6cd7e2e
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:06:02 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/amoeba/name.mli
@@ -0,0 +1,35 @@
+(*
+**      ==================================
+**      OOOO   OOOO OOOO  O      O   OOOO
+**      O   O  O    O     O     O O  O   O
+**      O   O  O    O     O     O O  O   O
+**      OOOO   OOOO OOOO  O     OOO  OOOO
+**      O   O     O    O  O    O   O O   O
+**      O   O     O    O  O    O   O O   O
+**      OOOO   OOOO OOOO  OOOO O   O OOOO
+**      ================================== 
+**      BSSLAB, Dr. Stefan Bosse sci@bsslab.de
+**
+**    PROTECTED BY AND DISTRIBUTED UNDER THE TERMS OF: 
+**    Free Software Foundation-Europe, GNU GPL License, Version 2
+**
+**    $MODIFIEDBY:  BSSLAB
+**    $AUTHORS:     Stefan Bosse
+**    $INITIAL:     (C) 2003 BSSLAB
+**    $CREATED:     
+**    $MODIFIED:    
+**    $VERSION:     1.02
+**
+**    $INFO:
+**
+** Name stub interfaces
+**  
+**
+**    $ENDOFINFO
+**
+*)
+
+
+val name_lookup : string -> Amoeba.status * Amoeba.capability
+val name_append : name:string -> cap:Amoeba.capability -> Amoeba.status
+val name_delete : string -> Amoeba.status
