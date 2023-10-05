babdc1efdfe988f759ce591b7425f693bad1b65f
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 17:50:20 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/xwin/vxlib/example/buttable5.ml
@@ -0,0 +1,88 @@
+open X
+open VX_types
+open VX_box
+open VX_text
+open VX_texttable
+open VX_ps 
+
+let display = new VX_display.t ""
+let root = new VX_root.t display 0 
+let top = new VX_wmtop.t root      
+          [MinHeight 200; MinWidth 300]
+
+let vbox = new VX_box.v top#container 
+                            [
+                                Border [];
+                                Background "lightblue";
+                                IpadX 10;
+                                IpadY 10;
+                                ExpandX true;
+                                ExpandY true;
+                                AdjustY true;
+                            ] 
+
+let hbox = new VX_box.h vbox#container 
+                            [
+                                Border [];
+                                MinWidth 100;
+                                ExpandX true;
+                                Background "white";
+                            ] 
+let closewindow () = 
+        Display.closeDisplay (top#display)
+
+let tab_cont = [|
+    [| "Start";"Exit";|];
+    [| "Stop";"Group1";|];
+    [| "Group2";"Group3";|];
+|]
+
+
+let attr = [
+    IpadX 20;
+    IpadY 5;
+    ExpandX true;
+    MinWidth 250;
+    But [
+        Shape S_Rect; Size 2;
+        Color "grey80";
+        Frame ReliefRaised];
+    Cols [|
+        [| 
+            [ExpandX true];
+            [ExpandX true;
+             ActionUU (fun () -> closewindow(); exit 0)];
+        |];
+        [| 
+            [ExpandX true];
+            [Group 1;Active true; ExpandX true];
+        |];
+        [| 
+            [Group 1;ExpandX true];
+            [Group 1;ExpandX true];
+        |];
+    |];
+]
+
+let tab = new VX_button.table hbox#container tab_cont attr  
+
+let _ =
+    hbox#container_add_s [tab#contained];
+    vbox#container_add_s [hbox#contained];
+
+    top#setWM_NAME "VX test";
+    top#container_add vbox#contained;
+    top#show;
+    let printed = ref false in
+    top#configure [Bindings [
+        EnterWindow,(fun _ ->
+            if not !printed then
+            begin
+                printed := true;
+                print_eps "buttable5.eps" 0.7 vbox#contained;
+            end;
+            );
+        ]];
+    try
+      loop ();
+    with Exit -> ();
