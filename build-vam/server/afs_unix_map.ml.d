6bc9d9c8d722e10d73a97a1c9d4677117e26c810
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:13:58 +0200

    Thu 05 Oct 2023 06:13:04 PM CEST

---
+++ build-vam/server/afs_unix_map.ml.d
@@ -0,0 +1,13 @@
+/amoeba/vam-2.0/src/amoeba/afs_common.mli
+/amoeba/vam-2.0/src/server/afs_server.mli
+/amoeba/vam-2.0/src/amoeba/amoeba.mli
+/amoeba/vam-2.0/src/amoeba/ar.mli
+/amoeba/vam-2.0/src/amoeba/buf.mli
+/amoeba/vam-2.0/src/amoeba/capset.mli
+/amoeba/vam-2.0/src/amoeba/cmdreg.mli
+/amoeba/vam-2.0/src/amoeba/dblist.mli
+/amoeba/vam-2.0/src/amoeba/name.mli
+/amoeba/vam-2.0/src/amoeba/rpc.mli
+/amoeba/vam-2.0/src/amoeba/stdcom.mli
+/amoeba/vam-2.0/src/amoeba/stdcom2.mli
+/amoeba/vam-2.0/src/amoeba/stderr.mli
