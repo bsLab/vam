dcdaba3f920d0f30bcba4e7329d36379f2f32eb3
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:07:22 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/shell/shell_dir.mli
@@ -0,0 +1,4 @@
+val dir : path:string -> format:string -> string
+val copy : src:string -> dst:string -> args:string -> string
+val del : string -> string -> string
+val mkdir : string -> string -> string
