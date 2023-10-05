60fddd17b6c72d8fe25caa10a8e93cb30d357118
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 17:56:35 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/ocaml/byterun/compact.h
@@ -0,0 +1,42 @@
+/*
+**      ==================================
+**      OOOO   OOOO OOOO  O      O   OOOO
+**      O   O  O    O     O     O O  O   O
+**      O   O  O    O     O     O O  O   O
+**      OOOO   OOOO OOOO  O     OOO  OOOO
+**      O   O     O    O  O    O   O O   O
+**      O   O     O    O  O    O   O O   O
+**      OOOO   OOOO OOOO  OOOO O   O OOOO
+**      ================================== 
+**      BSSLAB, Dr. Stefan Bosse sci@bsslab.de
+**
+**    PROTECTED BY AND DISTRIBUTED UNDER THE TERMS OF: 
+**    Free Software Foundation-Europe, GNU GPL License, Version 2
+**
+**    $MODIFIEDBY:  BSSLAB
+**    $AUTHORS:     Damien Doligez
+**    $INITIAL:     (C) 1996 INRIA
+**    $CREATED:     2001.12.07
+**    $MODIFIED:    2003.11.08
+**    $VERSION:     1.01
+**
+**    $INFO:
+**
+**  Heap compaction, part of the Gargabe Collector
+**
+**    $ENDOFINFO
+**
+*/
+
+#ifndef _compact_
+#define _compact_
+
+
+#include "config.h"
+#include "misc.h"
+
+extern void compact_heap (void);
+extern void compact_heap_maybe (void);
+
+
+#endif /* _compact_ */
