14bb98647dd7f4ae9ca0df508d5f2edf2a343c57
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:01:58 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/unix/setsid.c
@@ -0,0 +1,48 @@
+/*
+**      ==================================
+**      OOOO   OOOO OOOO  O      O   OOOO
+**      O   O  O    O     O     O O  O   O
+**      O   O  O    O     O     O O  O   O
+**      OOOO   OOOO OOOO  O     OOO  OOOO
+**      O   O     O    O  O    O   O O   O
+**      O   O     O    O  O    O   O O   O
+**      OOOO   OOOO OOOO  OOOO O   O OOOO
+**      ================================== 
+**      BSSLAB, Dr. Stefan Bosse www.bsslab.de
+**
+**    PROTECTED BY AND DISTRIBUTED UNDER THE TERMS OF: 
+**    Free Software Foundation-Europe, GNU GPL License, Version 2
+**
+**    $MODIFIEDBY:  BSSLAB
+**    $AUTHORS:     Xavier Leroy, Stefan Bosse
+**    $INITIAL:     (C) 1996 INRIA
+**    $CREATED:     
+**    $MODIFIED:    
+**    $VERSION:     1.01
+**
+**    $INFO:
+**
+**
+**
+**    $ENDOFINFO
+**
+*/
+
+
+#include <mlvalues.h>
+#include "unixsupport.h"
+#include <fail.h>
+
+#ifdef HAS_UNISTD
+#include <unistd.h>
+#endif
+
+CAMLprim value unix_setsid(value unit)
+{
+#ifdef HAS_SETSID
+  return Val_int(setsid());
+#else
+  invalid_argument("setsid not implemented");
+  return Val_unit;
+#endif
+}
