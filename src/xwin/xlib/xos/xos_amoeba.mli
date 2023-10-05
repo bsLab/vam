248d9403dbcc3681fb88d83d602abdea36166408
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 17:52:49 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/xwin/xlib/xos/xos_amoeba.mli
@@ -0,0 +1,15 @@
+(*
+** OS dependent layer. Limited to the kind of connection system
+** used to communicate with the X-Server. 
+**
+** Amoeba: virtual circuit connections
+** 
+*)
+
+
+(*
+** Init this module
+*)
+
+val init : unit -> unit
+
