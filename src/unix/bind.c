3f8b398baf6b30fae7a847fe5facffb7dc460191
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:01:30 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/unix/bind.c
@@ -0,0 +1,57 @@
+/*
+**      ==================================
+**      OOOO   OOOO OOOO  O      O   OOOO
+**      O   O  O    O     O     O O  O   O
+**      O   O  O    O     O     O O  O   O
+**      OOOO   OOOO OOOO  O     OOO  OOOO
+**      O   O     O    O  O    O   O O   O
+**      O   O     O    O  O    O   O O   O
+**      OOOO   OOOO OOOO  OOOO O   O OOOO
+**      ================================== 
+**      BSSLAB, Dr. Stefan Bosse www.bsslab.de
+**
+**    PROTECTED BY AND DISTRIBUTED UNDER THE TERMS OF: 
+**    Free Software Foundation-Europe, GNU GPL License, Version 2
+**
+**    $MODIFIEDBY:  BSSLAB
+**    $AUTHORS:     Xavier Leroy, Stefan Bosse
+**    $INITIAL:     (C) 1996 INRIA
+**    $CREATED:     
+**    $MODIFIED:    
+**    $VERSION:     1.01
+**
+**    $INFO:
+**
+**
+**
+**    $ENDOFINFO
+**
+*/
+
+
+#include <mlvalues.h>
+#include "unixsupport.h"
+#include <fail.h>
+
+#ifdef HAS_SOCKETS
+
+#include "socketaddr.h"
+  
+CAMLprim value unix_bind(value socket, value address)
+{
+  int ret;
+  union sock_addr_union addr;
+  socklen_param_type addr_len;
+
+  get_sockaddr(address, &addr, &addr_len);
+  ret = bind(Int_val(socket), &addr.s_gen, addr_len);
+  if (ret == -1) uerror("bind", Nothing);
+  return Val_unit;
+}
+
+#else
+
+CAMLprim value unix_bind(value socket, value address)
+{ invalid_argument("bind not implemented"); }
+  
+#endif
