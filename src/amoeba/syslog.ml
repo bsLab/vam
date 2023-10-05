308879f78624da67e34cf7ffb111c0f67b614684
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:05:08 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/amoeba/syslog.ml
@@ -0,0 +1,31 @@
+(*
+**      ==================================
+**      OOOO   OOOO OOOO  O      O   OOOO
+**      O   O  O    O     O     O O  O   O
+**      O   O  O    O     O     O O  O   O
+**      OOOO   OOOO OOOO  O     OOO  OOOO
+**      O   O     O    O  O    O   O O   O
+**      O   O     O    O  O    O   O O   O
+**      OOOO   OOOO OOOO  OOOO O   O OOOO
+**      ==================================
+**      BSSLAB, Dr. Stefan Bosse sci@bsslab.de
+**
+**    PROTECTED BY AND DISTRIBUTED UNDER THE TERMS OF:
+**    Free Software Foundation-Europe, GNU GPL License, Version 2
+**
+**    $AUTHORS:     Stefan Bosse
+**    $INITIAL:     (C) 2005 BSSLAB
+**    $CREATED:     30.4.2005
+**    $VERSION:     1.02
+**
+**    $INFO:
+**
+**  Compound module of Syslog_common and Syslog_client modules. See interfaces
+**  of these modules for details.
+**
+**    $ENDOFINFO
+*)
+
+include Syslog_common
+include Syslog_client
+
