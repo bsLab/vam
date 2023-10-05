31838834a349804209a50dbb5ad372797117ed30
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 17:50:42 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/xwin/vxlib/example/checkbox1.ml
@@ -0,0 +1,75 @@
+open X
+open VX_types
+open VX_box
+open VX_text
+open VX_checkbox
+open VX_ps 
+
+let display = new VX_display.t ""
+let root = new VX_root.t display 0 
+let top = new VX_wmtop.t root      
+          [MinHeight 200; MinWidth 300]
+
+let vbox = new VX_box.v top#container 
+                            [
+                                Border [];
+                                Background "lightblue";
+                                IpadX 10;
+                                IpadY 10;
+                                ExpandX true;
+                                ExpandY true;
+                                AdjustY true;
+                            ] 
+
+let hbox = new VX_box.h vbox#container 
+                            [
+                                AdjustX true;
+                                ExpandX true;
+                                Background "lightblue";
+
+                            ] 
+
+let labels = [|
+    "First";
+    "Second";
+    "Third";
+|]
+
+let attr = [
+    Border [];
+    IpadX 10;
+    IpadY 5;
+    Rows [|
+            [Active true];
+            [];
+            [Frame Flat];
+    |];
+
+]
+
+
+let cb = new VX_checkbox.h  hbox#container 
+                            labels 
+                            attr
+
+let _ =
+    hbox#container_add_s [cb#contained];
+    vbox#container_add_s [hbox#contained];
+
+    top#setWM_NAME "VX test";
+    top#container_add vbox#contained;
+    top#show;
+    let printed = ref false in
+    top#configure [Bindings [
+        EnterWindow,(fun _ ->
+            if not !printed then
+            begin
+                printed := true;
+                print_eps "checkbox1.eps" 0.7 vbox#contained;
+            end;
+            );   
+        ]];   
+
+    try
+      loop ();
+    with Exit -> ();
