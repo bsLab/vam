09403d61b2971f247ad434eece2ce9f2ef4145da
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:02:15 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/unix/pipe.c
@@ -0,0 +1,55 @@
+/*
+**      ==================================
+**      OOOO   OOOO OOOO  O      O   OOOO
+**      O   O  O    O     O     O O  O   O
+**      O   O  O    O     O     O O  O   O
+**      OOOO   OOOO OOOO  O     OOO  OOOO
+**      O   O     O    O  O    O   O O   O
+**      O   O     O    O  O    O   O O   O
+**      OOOO   OOOO OOOO  OOOO O   O OOOO
+**      ================================== 
+**      BSSLAB, Dr. Stefan Bosse www.bsslab.de
+**
+**    PROTECTED BY AND DISTRIBUTED UNDER THE TERMS OF: 
+**    Free Software Foundation-Europe, GNU GPL License, Version 2
+**
+**    $MODIFIEDBY:  BSSLAB
+**    $AUTHORS:     Xavier Leroy, Stefan Bosse
+**    $INITIAL:     (C) 1996 INRIA
+**    $CREATED:     
+**    $MODIFIED:    
+**    $VERSION:     1.01
+**
+**    $INFO:
+**
+**
+**
+**    $ENDOFINFO
+**
+*/
+
+
+#include <mlvalues.h>
+#include <alloc.h>
+#include <fail.h>
+#include "unixsupport.h"
+
+#ifdef HAS_PIPE
+
+CAMLprim value unix_pipe(void)
+{
+  int fd[2];
+  value res;
+  if (pipe(fd) == -1) uerror("pipe", Nothing);
+  res = alloc_small(2, 0);
+  Field(res, 0) = Val_int(fd[0]);
+  Field(res, 1) = Val_int(fd[1]);
+  return res;
+}
+
+#else
+CAMLprim value unix_pipe(void)
+{
+    invalid_argument("pipe not implemented");
+};
+#endif
