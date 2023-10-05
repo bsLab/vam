4aa9d07335fb72def1dd7ef58f26ddc72ee1bb68
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 17:51:21 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/xwin/vxlib/example/view1.ml
@@ -0,0 +1,98 @@
+open X
+open VX_types
+open VX_box
+open VX_text
+open VX_ps
+open VX_tree
+open Thread
+open Math
+open Printf
+
+let display = new VX_display.t ""
+let root = new VX_root.t display 0 
+let top = new VX_wmtop.t root      
+          [MinHeight 200; MinWidth 300]
+
+let vbox = new VX_box.v top#container 
+                            [
+                                Border [];
+                                Background "lightblue";
+                                IpadX 10;
+                                IpadY 10;
+                                ExpandX true;
+                                ExpandY true;
+                                AdjustY true;
+                            ] 
+let hbox = new VX_box.h vbox#container 
+                            [
+                                AdjustX true;
+                                ExpandX true;
+                                Background "lightblue";
+                            ] 
+ 
+let tree1 = new VX_tree.t hbox#container  
+                            [
+                                Background "white";
+                                IpadX 5;
+                                IpadY 5;
+                                But [
+                                    ActionSU (fun str ->
+                                        printf "Opening %s" str;
+                                        print_newline ();
+                                        );
+                                    ActionSU (fun str ->
+                                        printf "Closing %s" str;
+                                        print_newline ();
+                                        );
+                                    ActionSU (fun str ->
+                                        printf "Selected %s" str;
+                                        print_newline ();
+                                        );
+                                    ];
+                            ]
+
+let view1 = new VX_view.v hbox#container [
+                                Width 150;
+                                Height 90;
+                                Border [Size 2];
+                                IpadX 4;
+                                IpadY 4;
+                                Background "white";
+                            ]
+let _ =
+    hbox#container_add_s [view1#contained];
+    view1#container_add tree1#contained;  
+    vbox#container_add_s [hbox#contained];
+
+    let tree = tree1#get_root in
+    tree.b_label <- "Filesystem";
+    let b1 = tree1#new_node "Test" in
+    
+    let l1 = tree1#new_leaf "Entry1" in
+    let l2 = tree1#new_leaf "Entry2" in
+    let l3 = tree1#new_leaf "Main" in
+    let l4 = tree1#new_leaf "Last" in
+    tree1#tree_add_s tree [Leaf l3;(Node b1);Leaf l4]; 
+    tree1#tree_add_s b1 [Leaf l1;Leaf l2];
+    b1.b_opened <- false; 
+    tree.b_opened <- false;
+    tree1#update;
+
+    top#setWM_NAME "VX test";
+    top#container_add vbox#contained;
+    top#show;
+
+    let printed = ref false in
+    __(top#configure [Bindings [
+        EnterWindow,(fun _ -> 
+            if not !printed then
+            begin
+                printed := true;
+                print_eps "view1.eps" 1.0 vbox#contained;
+            end;
+            );
+        ]]);
+
+    try
+      loop ();
+    with Exit -> ();
