6a28432ee3d06426f3815c3fa112a702ac94384e
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:01:38 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/unix/symlink.c
@@ -0,0 +1,50 @@
+/*
+**      ==================================
+**      OOOO   OOOO OOOO  O      O   OOOO
+**      O   O  O    O     O     O O  O   O
+**      O   O  O    O     O     O O  O   O
+**      OOOO   OOOO OOOO  O     OOO  OOOO
+**      O   O     O    O  O    O   O O   O
+**      O   O     O    O  O    O   O O   O
+**      OOOO   OOOO OOOO  OOOO O   O OOOO
+**      ================================== 
+**      BSSLAB, Dr. Stefan Bosse www.bsslab.de
+**
+**    PROTECTED BY AND DISTRIBUTED UNDER THE TERMS OF: 
+**    Free Software Foundation-Europe, GNU GPL License, Version 2
+**
+**    $MODIFIEDBY:  BSSLAB
+**    $AUTHORS:     Xavier Leroy, Stefan Bosse
+**    $INITIAL:     (C) 1996 INRIA
+**    $CREATED:     
+**    $MODIFIED:    
+**    $VERSION:     1.01
+**
+**    $INFO:
+**
+**
+**
+**    $ENDOFINFO
+**
+*/
+
+
+#include <mlvalues.h>
+#include "unixsupport.h"
+#include <fail.h>
+
+#ifdef HAS_SYMLINK
+
+CAMLprim value unix_symlink(value path1, value path2)
+{
+  if (symlink(String_val(path1), String_val(path2)) == -1)
+    uerror("symlink", path2);
+  return Val_unit;
+}
+
+#else
+
+CAMLprim value unix_symlink(value path1, value path2)
+{ invalid_argument("symlink not implemented"); }
+
+#endif
