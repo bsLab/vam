a86246102cdea695d50506e7a8ec72140f2950a5
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:04:28 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/system/trace/io.ml
@@ -0,0 +1,41 @@
+
+open Amoeba
+open Name
+open Stderr
+open Stdcom
+open Cap_env
+
+
+let version = 0.61
+
+
+
+let stdin   = ref Pervasives.stdin
+let stdout  = ref Pervasives.stdout
+let stderr  = ref Pervasives.stderr
+ 
+let out str =
+    output_string !stdout str;
+    flush !stdout
+
+let err str =
+    output_string !stderr str;
+    flush !stderr
+    
+let nl () =
+    output_string !stdout "\n";
+    flush !stdout
+
+
+     
+        
+
+
+(*
+** Root and working directories
+*)
+
+let root_cap = ref (let _,cap = get_env_cap "ROOT" in cap)
+let file_cap = ref (let _,cap = get_env_cap "AFS" in cap)
+let work_dir = ref (Amoeba_path "/")
+let work_cap = ref (!root_cap)
