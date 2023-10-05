be03528cfe93f360bf9be92c99fd36aae3ab867c
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:01:45 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/unix/_unixLabels.ml
@@ -0,0 +1,19 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                           Objective Caml                            *)
+(*                                                                     *)
+(*               Jacques Garrigue, Kyoto University RIMS               *)
+(*                                                                     *)
+(*  Copyright 2001 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the GNU Library General Public License, with    *)
+(*  the special exception on linking described in file ../../LICENSE.  *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id: unixLabels.ml,v 1.3 2001/12/07 13:40:38 xleroy Exp $ *)
+
+(* Module [UnixLabels]: labelled Unix module *)
+
+let mod_unixLabels_ver = 1.0
+include Unix
