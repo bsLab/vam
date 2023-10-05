7ff551e9085c9bd1829d4838a2a08ac77f73bc7a
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 17:54:09 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/xwin/xlib/fonts.ml
@@ -0,0 +1,229 @@
+(*
+**      ==================================
+**      OOOO   OOOO OOOO  O      O   OOOO
+**      O   O  O    O     O     O O  O   O
+**      O   O  O    O     O     O O  O   O
+**      OOOO   OOOO OOOO  O     OOO  OOOO
+**      O   O     O    O  O    O   O O   O
+**      O   O     O    O  O    O   O O   O
+**      OOOO   OOOO OOOO  OOOO O   O OOOO
+**      ================================== 
+**      BSSLAB, Dr. Stefan Bosse sci@bsslab.de
+**
+**    PROTECTED BY AND DISTRIBUTED UNDER THE TERMS OF: 
+**    Free Software Foundation-Europe, GNU GPL License, Version 2
+**
+**    $MODIFIEDBY:  BSSLAB
+**    $AUTHORS:     Stefan Bosse
+**    $INITIAL:     (C) 2003 BSSLAB
+**    $CREATED:     2003.11.30
+**    $VERSION:     1.01
+**
+**    $INFO:
+**
+**  Standard Font families. Type convention:
+**
+**  <Times/Helvetica/Symbol/Courier>.
+**          <Roman/Bold/Italic/BoldItalic>.
+**              <s8/10/11/14/18/20/24>
+**
+**
+**    $ENDOFINFO
+**
+*)
+
+module Times =
+struct
+    module Roman =
+    struct
+        let s8 = "-*-times-medium-r-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-times-medium-r-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-times-medium-r-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-times-medium-r-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-times-medium-r-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-times-medium-r-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-times-medium-r-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-times-medium-r-*-*-24-*-*-*-*-*-*-*"
+    end
+    module Bold =
+    struct
+        let s8 = "-*-times-bold-r-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-times-bold-r-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-times-bold-r-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-times-bold-r-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-times-bold-r-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-times-bold-r-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-times-bold-r-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-times-bold-r-*-*-24-*-*-*-*-*-*-*"
+    end
+    module Italic =
+    struct
+        let s8 = "-*-times-medium-i-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-times-medium-i-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-times-medium-i-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-times-medium-i-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-times-medium-i-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-times-medium-i-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-times-medium-i-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-times-medium-i-*-*-24-*-*-*-*-*-*-*"
+    end
+    module BoldItalic =
+    struct
+        let s8 = "-*-times-bold-i-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-times-bold-i-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-times-bold-i-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-times-bold-i-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-times-bold-i-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-times-bold-i-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-times-bold-i-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-times-bold-i-*-*-24-*-*-*-*-*-*-*"
+    end
+end
+
+module Symbol =
+struct
+    module Roman =
+    struct
+        let s8 = "-*-symbol-medium-r-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-symbol-medium-r-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-symbol-medium-r-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-symbol-medium-r-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-symbol-medium-r-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-symbol-medium-r-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-symbol-medium-r-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-symbol-medium-r-*-*-24-*-*-*-*-*-*-*"
+    end
+    module Bold =
+    struct
+        let s8 = "-*-symbol-medium-r-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-symbol-medium-r-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-symbol-medium-r-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-symbol-medium-r-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-symbol-medium-r-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-symbol-medium-r-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-symbol-medium-r-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-symbol-medium-r-*-*-24-*-*-*-*-*-*-*"
+    end
+    module Italic =
+    struct
+        let s8 = "-*-symbol-medium-r-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-symbol-medium-r-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-symbol-medium-r-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-symbol-medium-r-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-symbol-medium-r-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-symbol-medium-r-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-symbol-medium-r-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-symbol-medium-r-*-*-24-*-*-*-*-*-*-*"
+    end
+    module BoldItalic =
+    struct
+        let s8 = "-*-symbol-medium-r-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-symbol-medium-r-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-symbol-medium-r-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-symbol-medium-r-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-symbol-medium-r-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-symbol-medium-r-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-symbol-medium-r-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-symbol-medium-r-*-*-24-*-*-*-*-*-*-*"
+    end
+end
+
+module Helvetica =
+struct
+    module Roman =
+    struct
+        let s8 = "-*-helvetica-medium-r-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-helvetica-medium-r-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-helvetica-medium-r-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-helvetica-medium-r-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-helvetica-medium-r-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-helvetica-medium-r-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-helvetica-medium-r-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-helvetica-medium-r-*-*-24-*-*-*-*-*-*-*"
+    end
+    module Bold =
+    struct
+        let s8 = "-*-helvetica-bold-r-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-helvetica-bold-r-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-helvetica-bold-r-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-helvetica-bold-r-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-helvetica-bold-r-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-helvetica-bold-r-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-helvetica-bold-r-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-helvetica-bold-r-*-*-24-*-*-*-*-*-*-*"
+    end
+    module Italic =
+    struct
+        let s8 = "-*-helvetica-medium-i-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-helvetica-medium-i-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-helvetica-medium-i-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-helvetica-medium-i-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-helvetica-medium-i-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-helvetica-medium-i-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-helvetica-medium-i-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-helvetica-medium-i-*-*-24-*-*-*-*-*-*-*"
+    end
+    module BoldItalic =
+    struct
+        let s8 = "-*-helvetica-bold-i-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-helvetica-bold-i-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-helvetica-bold-i-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-helvetica-bold-i-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-helvetica-bold-i-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-helvetica-bold-i-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-helvetica-bold-i-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-helvetica-bold-i-*-*-24-*-*-*-*-*-*-*"
+    end
+end
+
+module Courier =
+struct
+    module Roman =
+    struct
+        let s8 = "-*-courier-medium-r-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-courier-medium-r-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-courier-medium-r-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-courier-medium-r-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-courier-medium-r-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-courier-medium-r-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-courier-medium-r-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-courier-medium-r-*-*-24-*-*-*-*-*-*-*"
+    end
+    module Bold =
+    struct
+        let s8 = "-*-courier-bold-r-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-courier-bold-r-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-courier-bold-r-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-courier-bold-r-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-courier-bold-r-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-courier-bold-r-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-courier-bold-r-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-courier-bold-r-*-*-24-*-*-*-*-*-*-*"
+    end
+    module Italic =
+    struct
+        let s8 = "-*-courier-medium-o-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-courier-medium-o-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-courier-medium-o-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-courier-medium-o-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-courier-medium-o-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-courier-medium-o-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-courier-medium-o-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-courier-medium-o-*-*-24-*-*-*-*-*-*-*"
+    end
+    module BoldItalic =
+    struct
+        let s8 = "-*-courier-bold-o-*-*-8-*-*-*-*-*-*-*"
+        let s10 = "-*-courier-bold-o-*-*-10-*-*-*-*-*-*-*"
+        let s11 = "-*-courier-bold-o-*-*-11-*-*-*-*-*-*-*"
+        let s12 = "-*-courier-bold-o-*-*-12-*-*-*-*-*-*-*"
+        let s14 = "-*-courier-bold-o-*-*-14-*-*-*-*-*-*-*"
+        let s18 = "-*-courier-bold-o-*-*-18-*-*-*-*-*-*-*"
+        let s20 = "-*-courier-bold-o-*-*-20-*-*-*-*-*-*-*"
+        let s24 = "-*-courier-bold-o-*-*-24-*-*-*-*-*-*-*"
+    end
+end
+
+
+
+
