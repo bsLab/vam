45f7ea4774bbcf28f98088ff5f0bfc4ddb440283
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:05:43 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/amoeba/name.ml
@@ -0,0 +1,47 @@
+(*
+**      ==================================
+**      OOOO   OOOO OOOO  O      O   OOOO
+**      O   O  O    O     O     O O  O   O
+**      O   O  O    O     O     O O  O   O
+**      OOOO   OOOO OOOO  O     OOO  OOOO
+**      O   O     O    O  O    O   O O   O
+**      O   O     O    O  O    O   O O   O
+**      OOOO   OOOO OOOO  OOOO O   O OOOO
+**      ================================== 
+**      BSSLAB, Dr. Stefan Bosse sci@bsslab.de
+**
+**    PROTECTED BY AND DISTRIBUTED UNDER THE TERMS OF: 
+**    Free Software Foundation-Europe, GNU GPL License, Version 2
+**
+**    $MODIFIEDBY:  BSSLAB
+**    $AUTHORS:     Stefan Bosse
+**    $INITIAL:     (C) 2003 BSSLAB
+**    $CREATED:     
+**    $MODIFIED:    
+**    $VERSION:     1.02
+**
+**    $INFO:
+**
+** Name stub interfaces
+**  
+**
+**    $ENDOFINFO
+**
+*)
+
+
+
+
+
+open Amoeba
+open Dir
+
+let name_lookup name =
+    dir_lookup ~root:nilcap ~name:name
+
+
+let name_append ~name:name ~cap:cap =
+    dir_append ~root:nilcap ~name:name ~obj:cap            
+
+let name_delete name =
+    dir_delete ~root:nilcap ~name:name  
