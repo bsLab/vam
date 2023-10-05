cc7707e5e2bfc8d1edc0b87638df1218abf3674c
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 17:52:15 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/xwin/common/version.ml
@@ -0,0 +1,29 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                           xlib for Ocaml                            *)
+(*                                                                     *)
+(*       Fabrice Le Fessant, projet Para/SOR, INRIA Rocquencourt       *)
+(*                                                                     *)
+(*  Copyright 1998 Institut National de Recherche en Informatique et   *)
+(*  Automatique.  Distributed only by permission.                      *)
+(*                                                                     *)
+(***********************************************************************)
+
+let ocamllib = "/usr/local/lib/ocaml"
+
+let installdir = "/usr/local/lib/xlib/3.01-"
+
+let builder = "sbosse"
+
+let date = "Tue Sep 25 20:41:37 CEST 2001"
+
+let efuns_version = ""
+
+let efuns_lib = "/usr/local/lib/efuns/3.01-"
+
+let gwml_version = ""
+
+let gwml_lib = "/usr/local/lib/gwml/3.01-"
+
+let src_dir = "/usr/home/sbosse/caml/efuns-017+5"
+
