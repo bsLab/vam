29354b8ea63ca9cb533c7b18c467998aec2abc29
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:02:21 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/unix/fork.c
@@ -0,0 +1,51 @@
+/*
+**      ==================================
+**      OOOO   OOOO OOOO  O      O   OOOO
+**      O   O  O    O     O     O O  O   O
+**      O   O  O    O     O     O O  O   O
+**      OOOO   OOOO OOOO  O     OOO  OOOO
+**      O   O     O    O  O    O   O O   O
+**      O   O     O    O  O    O   O O   O
+**      OOOO   OOOO OOOO  OOOO O   O OOOO
+**      ================================== 
+**      BSSLAB, Dr. Stefan Bosse www.bsslab.de
+**
+**    PROTECTED BY AND DISTRIBUTED UNDER THE TERMS OF: 
+**    Free Software Foundation-Europe, GNU GPL License, Version 2
+**
+**    $MODIFIEDBY:  BSSLAB
+**    $AUTHORS:     Xavier Leroy, Stefan Bosse
+**    $INITIAL:     (C) 1996 INRIA
+**    $CREATED:     
+**    $MODIFIED:    
+**    $VERSION:     1.01
+**
+**    $INFO:
+**
+**
+**
+**    $ENDOFINFO
+**
+*/
+
+
+#include <mlvalues.h>
+#include <fail.h>
+#include "unixsupport.h"
+
+#ifdef HAS_FORK
+
+CAMLprim value unix_fork(value unit)
+{
+  int ret;
+  ret = fork();
+  if (ret == -1) uerror("fork", Nothing);
+  return Val_int(ret);
+}
+
+#else   
+CAMLprim value unix_fork(value unit)
+{
+    invalid_argument("fork not implemented");
+};
+#endif
