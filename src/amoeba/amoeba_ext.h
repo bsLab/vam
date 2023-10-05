a73d139d49d674b4085e3ef6878a970983e1ebf1
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:05:20 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/amoeba/amoeba_ext.h
@@ -0,0 +1,27 @@
+/* 
+** This file is part of the FIREBALL AMOEBA System.
+**
+** Written by:
+**               Stefan Bosse
+**               sbosse@physik.uni-bremen.de
+**
+** Last modified:
+**           24/03/02
+**
+** Changes:
+**
+**
+**
+** FIREBALL AMOEBA is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License as 
+** published by the Free Software Foundation; version 2.
+**
+** The FIREBALL AMOEBA is distributed in the hope that it will be usefull,
+** but WITHOUT ANY WARRANTY; without even implied warranty of 
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+*/
+
+value ext_hdr_new   (value unit);
+value ext_priv_new  (value unit);
+value ext_cap_new   (value unit);
