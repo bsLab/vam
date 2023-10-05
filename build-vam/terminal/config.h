aa941f87d58c5033d1593396c113ad95b9dbb83f
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:14:14 +0200

    Thu 05 Oct 2023 06:13:04 PM CEST

---
+++ build-vam/terminal/config.h
@@ -0,0 +1,8 @@
+/* config.h header created from Amakefile.sys */
+#ifndef _config_
+#define _config_
+#define HAVE_STRING_H 1
+#define HAVE_UNISTD_H 1
+#define STDC_HEADERS 1
+#define TERMCAP_FILE "/etc/termcap"
+#endif
