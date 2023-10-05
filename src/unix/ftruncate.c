49fb56d43350503e47204214f7e58786a4c48a93
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:02:37 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/unix/ftruncate.c
@@ -0,0 +1,50 @@
+/*
+**      ==================================
+**      OOOO   OOOO OOOO  O      O   OOOO
+**      O   O  O    O     O     O O  O   O
+**      O   O  O    O     O     O O  O   O
+**      OOOO   OOOO OOOO  O     OOO  OOOO
+**      O   O     O    O  O    O   O O   O
+**      O   O     O    O  O    O   O O   O
+**      OOOO   OOOO OOOO  OOOO O   O OOOO
+**      ================================== 
+**      BSSLAB, Dr. Stefan Bosse www.bsslab.de
+**
+**    PROTECTED BY AND DISTRIBUTED UNDER THE TERMS OF: 
+**    Free Software Foundation-Europe, GNU GPL License, Version 2
+**
+**    $MODIFIEDBY:  BSSLAB
+**    $AUTHORS:     Xavier Leroy, Stefan Bosse
+**    $INITIAL:     (C) 1996 INRIA
+**    $CREATED:     
+**    $MODIFIED:    
+**    $VERSION:     1.01
+**
+**    $INFO:
+**
+**
+**
+**    $ENDOFINFO
+**
+*/
+
+
+#include <mlvalues.h>
+#include "unixsupport.h"
+#include <fail.h>
+
+#ifdef HAS_TRUNCATE
+
+CAMLprim value unix_ftruncate(value fd, value len)
+{
+  if (ftruncate(Int_val(fd), Long_val(len)) == -1)
+    uerror("ftruncate", Nothing);
+  return Val_unit;
+}
+
+#else
+
+CAMLprim value unix_ftruncate(value fd, value len)
+{ invalid_argument("ftruncate not implemented"); }
+
+#endif
