3c31b341da3e6f5101883e2e42fa5f0110abff2a
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 17:51:06 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/xwin/vxlib/example/edit1.ml
@@ -0,0 +1,63 @@
+open X
+open VX_types
+open VX_box
+open VX_text
+open VX_ps 
+
+let display = new VX_display.t ""
+let root = new VX_root.t display 0 
+let top = new VX_wmtop.t root      
+          [MinHeight 200; MinWidth 300]
+
+let vbox = new VX_box.v top#container 
+                            [
+                                Border [];
+                                Background "lightblue";
+                                IpadX 10;
+                                IpadY 10;
+                                ExpandX true;
+                                ExpandY true;
+                                AdjustY true;
+                            ] 
+let hbox = new VX_box.h vbox#container 
+                            [
+                                AdjustX true;
+                                ExpandX true;
+                                Background "lightblue";
+                            ] 
+ 
+let str = "This is a test multiline text paragraph with several words and many more..."
+let text1 = new VX_text.edit hbox#container str 
+                            [
+                                Background "white";
+                                Border []; 
+                                IpadX 5;
+                                IpadY 5;
+                                Width 200;
+                                Text_font Fixed;
+                                Text_size 12;
+                                Rown 5;            
+                                Text_baseline  [Line L_Dotted];
+                            ]
+
+let _ =
+    hbox#container_add_s [text1#contained];
+    vbox#container_add_s [hbox#contained];
+
+    top#setWM_NAME "VX test";
+    top#container_add vbox#contained;
+    top#show;
+    let printed = ref false in
+    top#configure [Bindings [
+        EnterWindow,(fun _ ->
+            if not !printed then
+            begin
+                printed := true;
+                print_eps "edit1.eps" 1.0 vbox#contained;
+            end;
+            );   
+        ]];   
+
+    try
+      loop ();
+    with Exit -> ();
