3dfc6743f52ecaea0fd556620e62b1055bbe4f35
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 17:49:52 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/xwin/vxlib/example/print.ml
@@ -0,0 +1,6 @@
+    __(top#configure [Bindings [
+            GrabbedKey(XK.xk_Down,0),(fun _ ->
+                print_eps "nav.eps" 1.0 nav#contained;
+            );
+        ]]);                    
+
