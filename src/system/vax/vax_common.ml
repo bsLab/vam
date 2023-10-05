1301b4f8caff845f812e2f6dec11554b989e5727
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 18:03:58 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/system/vax/vax_common.ml
@@ -0,0 +1,56 @@
+(*
+**      ==================================
+**      OOOO   OOOO OOOO  O      O   OOOO
+**      O   O  O    O     O     O O  O   O
+**      O   O  O    O     O     O O  O   O
+**      OOOO   OOOO OOOO  O     OOO  OOOO
+**      O   O     O    O  O    O   O O   O
+**      O   O     O    O  O    O   O O   O
+**      OOOO   OOOO OOOO  OOOO O   O OOOO
+**      ================================== 
+**      BSSLAB, Dr. Stefan Bosse sci@bsslab.de
+**
+**    PROTECTED BY AND DISTRIBUTED UNDER THE TERMS OF: 
+**    Free Software Foundation-Europe, GNU GPL License, Version 2
+**
+**    $MODIFIEDBY:  BSSLAB
+**    $AUTHORS:     Stefan Bosse
+**    $INITIAL:     (C) 2004 BSSLAB
+**    $CREATED:     
+**    $MODIFIED:    
+**    $VERSION:     1.01
+**
+**    $INFO:
+**
+**
+**
+**    $ENDOFINFO
+**
+*)
+
+open Amoeba
+open Bytebuf
+open Afu_server
+
+
+type exec_kind = 
+    | Binary
+    | Bytecode      (* #!/.../vamrun *)
+    | Script        (* #!/.../vam *)
+
+    
+type location =
+    | Amoeba_loc
+    | Unix_loc
+
+
+let max_files = 5
+
+let buf_size = 30000
+
+(*
+** AFS-UNIX file mapper server
+*)
+let afu_server = ref nilafuserver
+
+
