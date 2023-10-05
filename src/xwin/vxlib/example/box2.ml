75b9ce31e6406e7f9198f7d5526a8bd49cd6bc22
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 17:51:29 +0200

    Thu 05 Oct 2023 05:46:29 PM CEST

---
+++ src/xwin/vxlib/example/box2.ml
@@ -0,0 +1,51 @@
+open X
+open VX_types
+open VX_box
+open VX_ps
+
+let display = new VX_display.t ""
+let root = new VX_root.t display 0 
+let top = new VX_wmtop.t root      
+          [MinHeight 300; MinWidth 500; Background "white"]
+
+let vbox = new VX_box.v top#container [Border [Size 10];
+                                       Background "green";
+                                       IpadX 10;
+                                       IpadY 10;
+                                       ExpandX true;
+                                        ] 
+
+let hbox1 = new VX_box.h vbox#container [Width 150;
+                                        Height 120;
+                                        Border [Size 2];
+                                        Background "yellow";
+                                        ]
+
+let hbox2 = new VX_box.h vbox#container [Width 100;
+                                        Height 100;
+                                        Border [Size 2];
+                                        Background "yellow";
+                                        ] 
+
+let _ =
+    vbox#container_add_s [hbox1#contained;  
+                          hbox2#contained];  
+
+    top#setWM_NAME "VX test";
+    top#container_add vbox#contained;
+    top#show;
+
+    let printed = ref false in
+    top#configure [Bindings [
+        EnterWindow,(fun _ -> 
+            if not !printed then
+            begin
+                printed := true;
+                print_eps "box2.eps" 0.5 vbox#contained;
+            end;
+            );
+        ]];
+
+    try
+      loop ();
+    with Exit -> ();
